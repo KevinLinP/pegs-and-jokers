965fbd7c457bb7be20a80b2ad6aec0cec360c24d
Kevin Lin <kevin.lin.p@gmail.com>
2021-03-02 23:25:12 -0500

    add indexes for Players and GameCards

--- server/main.js
+++ server/main.js
@@ -1,9 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { Players } from '../imports/api/players.js'
+import { GameCards } from '../imports/api/game-cards.js'
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Migrations.add({
+  version: 1,
+  up: function() {
+    Players.rawCollection().createIndex({gameId: 1, color: 1}, {unique: true})
+    Players.rawCollection().createIndex({gameId: 1, name: 1}, {unique: true})
+
+    GameCards.rawCollection().createIndex({gameId: 1, owner: 1})
+    GameCards.rawCollection().createIndex({gameId: 1, deck: 1, suit: 1, num: 1}, {unique: true})
+  }
 });
 
-'games.initializeState' () {
-  if (!this.userId) { return 'not logged in' }
-}
+Meteor.startup(() => {
+  Migrations.migrateTo('latest');
+});
