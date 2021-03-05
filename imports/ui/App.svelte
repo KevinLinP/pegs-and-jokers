f1dc79807eebc0b346c1bae0920ec43a60ca8836
Kevin Lin <kevin.lin.p@gmail.com>
2021-03-04 19:00:04 -0500

    move svg draw into draw-svg.js

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,122 +1,29 @@
 <script>
-  import _ from 'lodash'
-  import * as d3 from 'd3'
-  import { transform } from 'd3-transform'
   import { onMount } from 'svelte'
+
   import ToDo from './ToDo.svelte'
+  import drawSvg from './draw-svg.js'
 
   import panzoom from 'panzoom'
   import svgPanZoom from 'svg-pan-zoom'
 
   let svg
-  let numPlayers = 6
-  const playerColors = [
-    '#FF8C00',
-    '#00FFFF',
-    '#FF0000',
-    '#FFF68F',
-    '#00FF00',
-    '#ff00ff'
-  ]
-  let data = _.range(numPlayers).map((n) => {
-    return {num: n + 1, mainHoles: _.range(18), color: playerColors[n] || '#FFFFFF'}
-  })
-
-  function sinD(angle) {
-    return Math.sin(angle*Math.PI/180);
-  }
-  function cosD(angle) {
-    return Math.cos(angle*Math.PI/180);
-  }
-
-  const holeRadius = 0.5
-  const holeSpacing = 2.25
-  const sideLength = 18 * holeSpacing
-  const boardCircleRadius = sideLength / (2*sinD(180 / numPlayers))
-  const angle = (360 / numPlayers)
 
   onMount(() => {
-    var groupTransform = transform()
-      .translate(function (d) {
-        const circleAngle = (-1 * (d.num - 1 ) * angle) + (angle / 2)
-        const translate = [
-          sinD(circleAngle) * boardCircleRadius,
-          cosD(circleAngle) * boardCircleRadius,
-        ]
-        return translate
-      }).rotate(function (d) {
-        return ((d.num - 1) * angle)
-      })
-    
-    const board = d3.select(svg).append('g').attr('id', 'svg-board')
-
-    const group = board.selectAll('g')
-      .data(data)
-      .join('g')
-      .attr('transform', groupTransform)
-    
-    group.append('g')
-      .selectAll('circle')
-      .data(function (d, i) { return d.mainHoles})
-      .join('circle')
-      .attr('r', holeRadius)
-      .attr('cx', (d) => d * -1 * holeSpacing)
-      .attr('cy', 0)
-      .attr('fill', (d) => {
-        if (d == 0) {
-          return "#666666"
-        } else if (d == 3 || d == 8) {
-          return "#FFFFFF"
-        } else {
-          return "#FFFFFF"
-        }
-      })
-
-    const startPositions = [
-      [0, -1], [-1, 0], [0, 0], [1, 0], [0, 1]
+    const numPlayers = 6
+    const playerColors = [
+      '#FF8C00',
+      '#00FFFF',
+      '#FF0000',
+      '#FFF68F',
+      '#00FF00',
+      '#ff00ff'
     ]
 
-    const startTransform = transform().translate(-8 * holeSpacing, -2 * holeSpacing)
+    drawSvg(svg, {numPlayers, playerColors})
 
-    group.append('g')
-      .attr('transform', startTransform)
-      .selectAll('circle')
-      .data(_.range(5))
-      .join('circle')
-      .attr('r', holeRadius)
-      .attr('cx', function (d) { return (startPositions[d][0]) * holeSpacing})
-      .attr('cy', function (d) { return (startPositions[d][1]) * holeSpacing })
-      .attr('fill', function (d) { return this.parentNode.__data__.color })
-    
-    const homeTransform = transform().translate(-3 * holeSpacing, 0 * holeSpacing).rotate(-30)
-
-    group.append('g')
-      .attr('transform', homeTransform)
-      .selectAll('circle')
-      .data(_.range(5))
-      .join('circle')
-      .attr('r', holeRadius)
-      .attr('cx', 0)
-      .attr('cy', function (d) {return ((d * -1) - 1) * holeSpacing})
-      .attr('fill', function (d) { return this.parentNode.__data__.color })
-
-    // panzoom(document.getElementById('svg-board'), {
-    //   smoothScroll: false
-    // })
     svgPanZoom('#svg')
 	});
-
-  const todos = [
-    'animate moving peg',
-    'tweak panning/zooming',
-    'finish data model playing cards',
-    'data model pieces/positions',
-    'available moves calculator',
-    'display playing cards',
-    'undo/redo',
-    'lots more'
-  ]
-
 </script>
 
 <style>
